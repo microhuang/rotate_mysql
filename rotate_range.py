bcfdcff174a8b639b3666dba733c25af4c4af681
microhuang <microhuang@live.com>
2014-02-14 22:53:49 +0800

    Create rotate_range.py

---
+++ rotate_range.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#mysql partition range table
+
+
+import sys
+import getopt
+import re
+import MySQLdb
+
+import config.db_mapping
+
+
+def main():
+    
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
